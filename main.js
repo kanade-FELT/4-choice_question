7f35edf65c1c926df93c2139e753d8a7532e4361
kanade <uminomanekineko@gmail.com>
2023-06-10 22:08:10 +0900

    detach questions.js

--- main.js
+++ main.js
@@ -20,17 +20,17 @@
   var elapsedTime = 0;
   var holdTime = 0;
 
-  const p1_1 = "'pictures/study_gogaku_man1_english.png' id='1'";
-  const p1_2 = "'pictures/study_gogaku_man2_spanish.png'";
-  const p1_3 = "'pictures/study_gogaku_man3_french.png'";
-  const p1_4 = "'pictures/study_gogaku_man4_chinese.png'";
-  const ans1 = '1';
+  // const p1_1 = "'pictures/study_gogaku_man1_english.png' id='1'";
+  // const p1_2 = "'pictures/study_gogaku_man2_spanish.png'";
+  // const p1_3 = "'pictures/study_gogaku_man3_french.png'";
+  // const p1_4 = "'pictures/study_gogaku_man4_chinese.png'";
+  // const ans1 = '1';
 
-  const p2_1 = "'pictures/study_gogaku_man1_english.png' id='1'";
-  const p2_2 = "'pictures/study_gogaku_man2_spanish.png'";
-  const p2_3 = "'pictures/study_gogaku_man3_french.png'";
-  const p2_4 = "'pictures/study_gogaku_man4_chinese.png'";
-  const ans2 = '1';
+  // const p2_1 = "'pictures/study_gogaku_man1_english.png' id='1'";
+  // const p2_2 = "'pictures/study_gogaku_man2_spanish.png'";
+  // const p2_3 = "'pictures/study_gogaku_man3_french.png'";
+  // const p2_4 = "'pictures/study_gogaku_man4_chinese.png'";
+  // const ans2 = '1';
 
   //問題文を格納
   const question = [
---
+++ questions.js
@@ -0,0 +1,13 @@
+
+
+const p1_1 = "'pictures/study_gogaku_man1_english.png' id='1'";
+const p1_2 = "'pictures/study_gogaku_man2_spanish.png'";
+const p1_3 = "'pictures/study_gogaku_man3_french.png'";
+const p1_4 = "'pictures/study_gogaku_man4_chinese.png'";
+const ans1 = '1';
+
+const p2_1 = "'pictures/study_gogaku_man1_english.png' id='1'";
+const p2_2 = "'pictures/study_gogaku_man2_spanish.png'";
+const p2_3 = "'pictures/study_gogaku_man3_french.png'";
+const p2_4 = "'pictures/study_gogaku_man4_chinese.png'";
+const ans2 = '1';
