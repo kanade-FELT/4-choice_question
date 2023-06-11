f413e7203f385859cab87232470d9cddde6d2cb1
kanade <uminomanekineko@gmail.com>
2023-06-11 10:34:39 +0900

    Demo

--- questions.js
+++ questions.js
@@ -24,4 +24,31 @@ const question = [
       'pictures/study_gogaku_man4_chinese.png'
     ],
   },
+  {
+    text: "2.英語の勉強をしているのは誰？",
+    choice: [
+      'pictures/study_gogaku_man1_english.png',  // correct answer
+      'pictures/study_gogaku_man2_spanish.png',
+      'pictures/study_gogaku_man3_french.png',
+      'pictures/study_gogaku_man4_chinese.png'
+    ],
+  },
+  {
+    text: "3.英語の勉強をしているのは誰？",
+    choice: [
+      'pictures/study_gogaku_man1_english.png',  // correct answer
+      'pictures/study_gogaku_man2_spanish.png',
+      'pictures/study_gogaku_man3_french.png',
+      'pictures/study_gogaku_man4_chinese.png'
+    ],
+  },
+  {
+    text: "4.英語の勉強をしているのは誰？",
+    choice: [
+      'pictures/study_gogaku_man1_english.png',  // correct answer
+      'pictures/study_gogaku_man2_spanish.png',
+      'pictures/study_gogaku_man3_french.png',
+      'pictures/study_gogaku_man4_chinese.png'
+    ],
+  },
 ];
